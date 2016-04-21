db088384bb20b7d99f9b5503ec29a5be98bcfe32
Myeongjin <aranet100@gmail.com>
2016-04-21 16:03:48 +0900

    translation update

---
+++ lang/ko/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Korean language file for config
+ *
+ */
+
+$lang['discussionPage']   = '토론 문서 (토론을 비활성화하려면 비워 두세요)';
+$lang['userPage']         = '사용자 문서 (사용자 문서를 비활성화하려면 비워 두세요)';
+$lang['hideTools']        = '로그인하지 않을 때 도구를 숨길까요?';
+$lang['menuID']           = 'nav-플러그인이 생성한 메뉴의 ID';
+$lang['footerID']         = '바닥글 문서의 ID';
+$lang['headerImg']        = '다른 머리글 그림의 URL (비워 두면 기본값)';
